972943e3ae95e94a76e6c152bb0c43496c6f9b6c
takanari <taka0824nari@gmail.com>
2021-02-05 16:07:06 +0900

    ArrayExample02.java

---
+++ src/ArrayExample02.java
@@ -0,0 +1,11 @@
+
+public class ArrayExample02 {
+	public static void main(String[] args) {
+		int[] scores = {50, 55, 80, 75, 65};
+		int n = scores.length;
+		
+		for(int i = 0; i < n; i++) {
+			System.out.println(scores[i]);
+		}
+	}
+}
