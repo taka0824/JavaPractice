532a4e5c9bb2a000e19431616e4d86903b9e82b6
takanari <taka0824nari@gmail.com>
2021-02-05 16:03:55 +0900

    ArrayExample.java

--- src/TimeTable.java
+++ src/TimeTable.java
@@ -3,7 +3,8 @@ public class TimeTable {
 	public static void main(String[] args) {
 		for(int i = 1; i <= 9; i ++) {
 			for(int j = 1; j <= 9; j++) {
-				System.out.print(i + "x" + j + "=" + i * j);
+				int value = i * j;
+				System.out.print(i + "x" + j + "=" + value);
 				System.out.print("  ");
 			}
 			System.out.println("");
