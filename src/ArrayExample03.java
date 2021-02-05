b32e2425863e7cdc6b8df54c70750a2eae43baaf
takanari <taka0824nari@gmail.com>
2021-02-05 16:20:30 +0900

    ArrayExample03.java editted

--- src/ArrayExample03.java
+++ src/ArrayExample03.java
@@ -4,12 +4,13 @@ public class ArrayExample03 {
 		int[][] scores = {{50,55,60,55,75},
 				{65,85,90,95,88},
 				{30,15,25,55,60}
-				
+
 		};
-		
+
 		int l = scores.length;
+//		l = 配列の中の要素の数
 		int m = scores[1].length;
-		
+//		m = 配列の中の配列(scores[1])の中の要素の数
 		for(int i = 0; i < l; i++) {
 			System.out.print((i + 1) + "学期のスコア:");
 			for(int j = 0; j < m; j++) {
