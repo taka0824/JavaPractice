ace94eacc1b3fc644a8f375a21cf82deab669e8c
takanari <taka0824nari@gmail.com>
2021-02-05 15:28:42 +0900

    whileExample.java

---
+++ src/WhileExample.java
@@ -0,0 +1,15 @@
+
+public class WhileExample {
+	public static void main(String[] args) {
+		System.out.println("------------while-------------");
+		int i = 5;
+		while(i >= 1){
+			System.out.println(i);
+			i --;
+		}
+		System.out.println("------------for-------------");
+		for(int j = 5; j > 0; j --) {
+			System.out.println(j);
+		}
+	}
+}
