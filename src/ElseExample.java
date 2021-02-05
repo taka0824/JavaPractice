ccb884e72e986291536bd4997485a9eec0fd0013
takanari <taka0824nari@gmail.com>
2021-02-05 14:55:31 +0900

    if and else sentence

---
+++ src/ElseExample.java
@@ -0,0 +1,13 @@
+
+public class ElseExample {
+	public static void main(String[] args) {
+		int age;
+		age = 19;
+		
+		if(age < 20) {
+			System.out.println("未成年ですね");
+		} else {
+			System.out.println("投票にいきましょう");
+		}
+	}
+}
---
+++ src/IfExample.java
@@ -0,0 +1,12 @@
+
+public class IfExample {
+	public static void main(String[] args) {
+		int age;
+		age = 18;
+
+		if(age < 20) {
+			System.out.println("未成年ですね");
+		}
+	}
+
+}
