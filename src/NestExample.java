b1bb463914a3ebf7244a96de551b53ab6e6c3ec6
takanari <taka0824nari@gmail.com>
2021-02-05 15:51:44 +0900

    NestExample.java

---
+++ src/NestExample.java
@@ -0,0 +1,11 @@
+
+public class NestExample {
+	public static void main(String[] args) {
+		for(int a = 1; a < 4; a ++) {
+			System.out.println("a = " + a);
+			for(int b = 1; b < 4; b++) {
+				System.out.println("  b = " + b);
+			}
+		}
+	}
+}
---
+++ src/continueExample.java
@@ -0,0 +1,13 @@
+
+public class continueExample {
+	public static void main(String[] args) {
+		int sum = 0;
+		for(int i = 1; i <= 10; i++) {
+			if(i % 2 == 0) {
+				continue;
+			}
+			sum += i;
+			System.out.println("変数sumに" + i + "を加えました。sumは" + sum);
+		}
+	}
+}
