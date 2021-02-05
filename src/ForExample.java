6a14e8a1a927d569393fd62c263e8492bf99cd27
takanari <taka0824nari@gmail.com>
2021-02-05 15:17:51 +0900

    forExample.java

---
+++ src/ForExample.java
@@ -0,0 +1,10 @@
+
+public class ForExample {
+	public static void main(String[] args) {
+		for(int i = 0; i < 5; i++) {
+			System.out.println("forループ内の処理です");
+		}
+		System.out.println("ループ処理を終えました。");
+	}
+
+}
---
+++ src/LogicalSumExample.java
@@ -0,0 +1,13 @@
+
+public class LogicalSumExample {
+	public static void main(String[] args) {
+		int age = 11;
+		
+		if(age < 13 || age >= 65) {
+			System.out.println("入場料は無料です");
+		} else {
+			System.out.println("入場料は無料です");
+		}
+	}
+
+}
