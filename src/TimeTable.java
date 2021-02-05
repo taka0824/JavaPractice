4abb2cdeb1fed8b470f7a1e7ae0ae4eb35c16480
takanari <taka0824nari@gmail.com>
2021-02-05 15:55:21 +0900

    TimeTable.java

--- src/TimeTable.java
+++ src/TimeTable.java
@@ -1,4 +1,12 @@
 
 public class TimeTable {
-
+	public static void main(String[] args) {
+		for(int i = 1; i <= 9; i ++) {
+			for(int j = 1; j <= 9; j++) {
+				System.out.print(i + "x" + j + "=" + i * j);
+				System.out.print("  ");
+			}
+			System.out.println("");
+		}
+	}
 }
