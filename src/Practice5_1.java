949cf5d2d61d8a3eb50ec62fceaa97675e2290c5
takanari <taka0824nari@gmail.com>
2021-02-06 19:13:18 +0900

    Practice5_1.java

---
+++ src/Practice5_1.java
@@ -0,0 +1,13 @@
+
+public class Practice5_1 {
+	public static void main(String[] args) {
+		introductionOneself("西尾", 27, 183.5, "男");
+	}
+	
+	public static void introductionOneself(String name, int age, double height, String gender) {
+		System.out.println("名前：" + name);
+		System.out.println("年齢：" + age);
+		System.out.println("身長：" + height);
+		System.out.println("名前：" + gender);
+	}
+}
